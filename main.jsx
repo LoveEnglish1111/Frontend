67dbe4d90552c7fee8654131fa5e02975b0a1a7c
Xcode Hanji <truongcanhchan@gmail.com>
2026-01-29 10:54:21 +0700

    Fix file structer

--- main.jsx
+++ main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from '../App.jsx'
+import App from './src/App.jsx'
 
+console.log("Hello world");
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
